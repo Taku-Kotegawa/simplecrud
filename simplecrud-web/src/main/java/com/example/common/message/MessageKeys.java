2d58b9dc79c716004729fce36163bf0981c0c94d
taku.kotegawa <taku.kotegawa@gmail.com>
2020-04-07 19:17:48 +0900

    メッセージキー定数クラスの自動生成ツールの追加

---
+++ simplecrud-web/src/main/java/com/example/common/message/MessageKeys.java
@@ -0,0 +1,54 @@
+package com.example.common.message;
+/**
+ * Message Id
+ */
+public class MessageKeys {
+    /** e.xx.fw.5001=Resource not found. */
+    public static final String E_XX_FW_5001 = "e.xx.fw.5001";
+    /** e.xx.fw.7001=Illegal screen flow detected! */
+    public static final String E_XX_FW_7001 = "e.xx.fw.7001";
+    /** e.xx.fw.7002=CSRF attack detected! */
+    public static final String E_XX_FW_7002 = "e.xx.fw.7002";
+    /** e.xx.fw.7003=Access Denied detected! */
+    public static final String E_XX_FW_7003 = "e.xx.fw.7003";
+    /** e.xx.fw.7004=Missing CSRF detected! */
+    public static final String E_XX_FW_7004 = "e.xx.fw.7004";
+    /** e.xx.fw.8001=Business error occurred! */
+    public static final String E_XX_FW_8001 = "e.xx.fw.8001";
+    /** e.xx.fw.9001=System error occurred! */
+    public static final String E_XX_FW_9001 = "e.xx.fw.9001";
+    /** e.xx.fw.9002=Data Access error! */
+    public static final String E_XX_FW_9002 = "e.xx.fw.9002";
+    /** title.welcome.home=welcome home */
+    public static final String TITLE_WELCOME_HOME = "title.welcome.home";
+    /** typeMismatch="{0}" is invalid. */
+    public static final String TYPEMISMATCH = "typeMismatch";
+    /** typeMismatch.int="{0}" must be an integer. */
+    public static final String TYPEMISMATCH_INT = "typeMismatch.int";
+    /** typeMismatch.double="{0}" must be a double. */
+    public static final String TYPEMISMATCH_DOUBLE = "typeMismatch.double";
+    /** typeMismatch.float="{0}" must be a float. */
+    public static final String TYPEMISMATCH_FLOAT = "typeMismatch.float";
+    /** typeMismatch.long="{0}" must be a long. */
+    public static final String TYPEMISMATCH_LONG = "typeMismatch.long";
+    /** typeMismatch.short="{0}" must be a short. */
+    public static final String TYPEMISMATCH_SHORT = "typeMismatch.short";
+    /** typeMismatch.boolean="{0}" must be a boolean. */
+    public static final String TYPEMISMATCH_BOOLEAN = "typeMismatch.boolean";
+    /** typeMismatch.java.lang.Integer="{0}" must be an integer. */
+    public static final String TYPEMISMATCH_JAVA_LANG_INTEGER = "typeMismatch.java.lang.Integer";
+    /** typeMismatch.java.lang.Double="{0}" must be a double. */
+    public static final String TYPEMISMATCH_JAVA_LANG_DOUBLE = "typeMismatch.java.lang.Double";
+    /** typeMismatch.java.lang.Float="{0}" must be a float. */
+    public static final String TYPEMISMATCH_JAVA_LANG_FLOAT = "typeMismatch.java.lang.Float";
+    /** typeMismatch.java.lang.Long="{0}" must be a long. */
+    public static final String TYPEMISMATCH_JAVA_LANG_LONG = "typeMismatch.java.lang.Long";
+    /** typeMismatch.java.lang.Short="{0}" must be a short. */
+    public static final String TYPEMISMATCH_JAVA_LANG_SHORT = "typeMismatch.java.lang.Short";
+    /** typeMismatch.java.lang.Boolean="{0}" is not a boolean. */
+    public static final String TYPEMISMATCH_JAVA_LANG_BOOLEAN = "typeMismatch.java.lang.Boolean";
+    /** typeMismatch.java.util.Date="{0}" is not a date. */
+    public static final String TYPEMISMATCH_JAVA_UTIL_DATE = "typeMismatch.java.util.Date";
+    /** typeMismatch.java.lang.Enum="{0}" is not a valid value. */
+    public static final String TYPEMISMATCH_JAVA_LANG_ENUM = "typeMismatch.java.lang.Enum";
+}
