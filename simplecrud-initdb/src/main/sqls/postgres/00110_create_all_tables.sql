7ad537bf21d64bb9d0f423f631c4b90b5b6b0e5d
taku.kotegawa <taku.kotegawa@gmail.com>
2020-04-12 10:05:20 +0900

    テーブルにNOT NULLを追加

--- simplecrud-initdb/src/main/sqls/postgres/00110_create_all_tables.sql
+++ simplecrud-initdb/src/main/sqls/postgres/00110_create_all_tables.sql
@@ -1,12 +1,14 @@
+drop table users;
+
 -- User テーブル
 create table if not exists  users (
     uid varchar(60) primary key,
-    name varchar(60),
-    pass varchar(255),
-    mail varchar(254),
-    status boolean,
-    created_at timestamp,
-    changed_at timestamp,
+    name varchar(60) NOT NULL,
+    pass varchar(255) NOT NULL,
+    mail varchar(254) NOT NULL,
+    status boolean NOT NULL,
+    created_at timestamp NOT NULL,
+    changed_at timestamp NOT NULL,
     comment text
 )
 ;
@@ -20,3 +22,5 @@ COMMENT ON COLUMN users.mail IS 'メール';
 COMMENT ON COLUMN users.status IS 'ステータス';
 COMMENT ON COLUMN users.created_at IS '作成日時';
 COMMENT ON COLUMN users.changed_at IS '更新日時';
+COMMENT ON COLUMN users.comment IS 'コメント';
+
